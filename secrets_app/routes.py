937c5747d6ca58fe49a72c86c907e115ad3ffda6
ravilladhaneesh <ravilladhaneesh@gmail.com>
2025-01-09 23:25:47 +0530

    Add user registration and login implementation

--- secrets_app/routes.py
+++ secrets_app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, request, redirect, flash
 from secrets_app.forms import UserLoginForm, UserRegistrationForm
-from secrets_app import app
+from secrets_app import app, db, bcrypt
+from secrets_app.model import User
 
 @app.route("/")
 def home():
@@ -12,11 +13,14 @@ def register():
     if request.method == "POST":
         print(form.validate_on_submit() == True)
         if form.validate_on_submit():
-            firstName = form.firstName.data
-            flash(f'Account created for user {firstName}!', 'success')
+            hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            user = User(firstName=form.firstName.data, lastName=form.lastName.data, email=form.email.data, password=hashed_pw)
+            db.session.add(user)
+            db.session.commit()
+            flash(f'Account created for email {form.email.data}!.You can now login in.', 'success')
             return redirect(url_for('login'))
         else:
-            flash(f'Error Validating', 'warning')
+            flash(f'Error Validating the form', 'danger')
     return render_template("register.html", form=form, title='register')
 
 
@@ -25,12 +29,11 @@ def login():
     form = UserLoginForm()
     if request.method == "POST":
         if form.validate_on_submit():
-            email = form.email.data
-            print(email)
-            flash(f'Logged In as {email}', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login failed. Please check username and password', 'danger')
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                flash(f'Logged In as {user.firstName}', 'success')
+                return redirect(url_for('home'))
+        flash('Login failed. Please check username and password', 'danger')
     return render_template("login.html", form=form, title='login')
 
 
